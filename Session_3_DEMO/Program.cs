c667ae400e564982a679399d04a67f42f53039fa
Bayan <Bayan@DESKTOP-COKKPVL>
2024-07-14 17:43:54 +0300

    Add project files.

---
+++ Session_3_DEMO/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_3_DEMO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

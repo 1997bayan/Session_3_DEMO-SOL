4cb0b4b203dd77abd492ffd26637df7b05819c54
Bayan <Bayan@DESKTOP-COKKPVL>
2024-07-14 21:09:54 +0300

    Binding

--- Session_3_DEMO/Program.cs
+++ Session_3_DEMO/Program.cs
@@ -21,6 +21,21 @@ namespace Session_3_DEMO
             typeB.MyFun01();
             typeB.MyFun02();
 
+            #endregion
+
+            #region Binding
+            //Ref from parent => point on => Object from child
+            TypeA refBase;
+            //Declare for ref from Type "TypeA"
+            //refBase Refer to am object from Type "TypeA"
+            // Or refer to an object from any class [inherit from TypeA]
+
+            refBase = new TypeB(1, 3); // ==> this is binding
+            refBase.A = 120;
+            //refBase.B = 1233;// Invalid: B is not accessible from TypeA reference
+
+            refBase.MyFun01 ();// I am Base [Parent] // The hidden method MyFun01 is called from the TypeA implementation when accessed through a TypeA reference.
+            refBase.MyFun02();// TypeB : A = 120 , B = 3 //The overridden method MyFun02 is called from the TypeB implementation.
 
 
 

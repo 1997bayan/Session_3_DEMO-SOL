8b8a187acff42524df9b2f563ceb9a3043ffaf23
Bayan <Bayan@DESKTOP-COKKPVL>
2024-07-15 13:29:16 +0300

    Binding EX01

---
+++ Session_3_DEMO/Binding/PartTimeEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Session_3_DEMO.Binding
+{
+    internal class PartTimeEmployee :Employee
+    {
+        public int CountOfHours { get; set; }
+        public decimal HourRate { get; set; }
+
+
+        public new void GetEmployeeType()
+        {
+            Console.WriteLine("I am part time Employee");
+
+        }
+
+        public override void GetEmployeeData()
+        {
+            Console.WriteLine($"Full time Employee : Id = {Id} , Name = {Name} , Age = {Age} ,Count Of Hours = {CountOfHours} , Hour Rate = {HourRate}  ");
+        }
+
+
+    }
+}
